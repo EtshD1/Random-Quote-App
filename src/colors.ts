a46aaf4711cda9a6573ebea825c3083adb0f27a9
EtshD1 <H79Pain@Gmail.com>
2021-04-29 03:18:09 +0200

    add colors

---
+++ src/colors.ts
@@ -0,0 +1,60 @@
+const colors = [
+  {
+    color: "1E2019",
+    whiteFont: true
+  },
+  {
+    color: "587B7F",
+    whiteFont: true
+  },
+  {
+    color: "CFEE9E",
+    whiteFont: false
+  },
+  {
+    color: "DC136C",
+    whiteFont: true
+  },
+  {
+    color: "A2A392",
+    whiteFont: false
+  },
+  {
+    color: "9A998C",
+    whiteFont: true
+  },
+  {
+    color: "7AC74F",
+    whiteFont: false
+  },
+  {
+    color: "E0C879",
+    whiteFont: false
+  },
+  {
+    color: "E87461",
+    whiteFont: true
+  },
+  {
+    color: "19535F",
+    whiteFont: true
+  },
+  {
+    color: "D7C9AA",
+    whiteFont: false
+  },
+  {
+    color: "5C415D",
+    whiteFont: true
+  },
+  {
+    color: "F6F7EB",
+    whiteFont: false
+  },
+  {
+    color: "0E1428",
+    whiteFont: true
+  }
+];
+
+export default colors;
