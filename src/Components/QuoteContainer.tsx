66d748e3a55e78baaf2938ebe2060b59b26cd416
EtshD1 <H79Pain@Gmail.com>
2021-04-29 04:35:56 +0200

    add github profile link

--- src/Components/QuoteContainer.tsx
+++ src/Components/QuoteContainer.tsx
@@ -16,12 +16,17 @@ interface contextType {
 const QuoteContainer = ({ context }: contextType) => {
   const { currentColor } = context;
 
-  return (<div className={Styles.quoteContainer} id="quote-box" style={{ backgroundColor: `#${currentColor.color}` }}>
-    <div style={{ backgroundColor: currentColor.whiteFont ? "#fff" : "#000" }}>
-      <Quote />
-      <Actions />
+  return (<>
+    <div className={Styles.quoteContainer} id="quote-box" style={{ backgroundColor: `#${currentColor.color}` }}>
+      <div>
+        <div style={{ backgroundColor: currentColor.whiteFont ? "#fff" : "#000" }}>
+          <Quote />
+          <Actions />
+        </div>
+        <div className={Styles.madeBy} style={{ color: currentColor.whiteFont ? "#fff" : "#000" }}>by <a href="https://github.com/EtshD1">EtshD1</a></div>
+      </div>
     </div>
-  </div>);
+  </>);
 }
 
 export default withContext(QuoteContainer);
